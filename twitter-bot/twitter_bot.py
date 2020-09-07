96fd5ca78fa4f8fceac73deea19ae11accca121c
Em Elkan <35890264+EmElkan@users.noreply.github.com>
2020-09-07 21:11:13 +0100

    Update twitter_bot.py

--- twitter-bot/twitter_bot.py
+++ twitter-bot/twitter_bot.py
@@ -2,13 +2,9 @@ import tweepy
 import time
 import os
 
-CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
-CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
-ACCESS_KEY = os.environ.get("ACCESS_KEY")
-ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(CONSUMER_KEY = os.environ.get("CONSUMER_KEY"), CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET"))
+auth.set_access_token(ACCESS_KEY = os.environ.get("ACCESS_KEY"), ACCESS_SECRET = os.environ.get("ACCESS_SECRET"))
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 for tweet in tweepy.Cursor(api.search,
