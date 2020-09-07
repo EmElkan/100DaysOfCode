dd10e55c7168913e010b5019e62b0d32e771e337
Em Elkan <35890264+EmElkan@users.noreply.github.com>
2020-09-07 20:32:10 +0100

    Update twitter_bot.py

--- twitter-bot/twitter_bot.py
+++ twitter-bot/twitter_bot.py
@@ -2,8 +2,8 @@ import tweepy
 import time
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler($CONSUMER_KEY, $CONSUMER_SECRET)
+auth.set_access_token($ACCESS_KEY, $ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
