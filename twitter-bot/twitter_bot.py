2ae16dac3fcbe287a6e084519ddb255682a9b689
Em Elkan <35890264+EmElkan@users.noreply.github.com>
2020-09-07 21:00:06 +0100

    Update twitter_bot.py

--- twitter-bot/twitter_bot.py
+++ twitter-bot/twitter_bot.py
@@ -1,11 +1,16 @@
 import tweepy
 import time
+import os
 
+
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+CONSUMER_SECRET = os.environ["ACCESS_KEY"]
+CONSUMER_SECRET = os.environ["ACCESS_SECRET"]
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
 
 for tweet in tweepy.Cursor(api.search,
                            q="#controlroom AND #callhandler OR #callhandlers OR #emergencyservices OR #what3words OR #controlroomawards",
