30f37eba0bdd2c23c872dc517e8e3514cd377924
Em Elkan <35890264+EmElkan@users.noreply.github.com>
2020-12-25 17:19:42 +0000

    Update twitter_bot.py

--- twitter-bot/twitter_bot.py
+++ twitter-bot/twitter_bot.py
@@ -13,7 +13,7 @@ auth.set_access_token(ACC_KEY, ACC_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 for tweet in tweepy.Cursor(api.search,
-                           q="#ControlRoomRevolution OR #ChristmasControlRooms OR #callhandlers OR #RBFRSChristmasCountdown OR #Teamlas OR #callhandler OR #fireandrescue",
+                           q="#ControlRoomRevolution OR #ChristmasControlRooms OR #callhandlers OR #RBFRSChristmasCountdown OR #callhandler OR #fireandrescue",
                            lang="en").items(10):
     try:
         print('Tweet successful')
